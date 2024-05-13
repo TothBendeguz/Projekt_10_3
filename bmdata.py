c712c22e3d566a6bad6ca31fe344c502abfee6f2
Bendegúz Tóth <toth.bendeguz@students.jedlik.eu>
2024-05-13 14:06:10 +0200

    Folytatás python

--- bmdata.py
+++ bmdata.py
@@ -11,4 +11,16 @@ def nap(ev):
     napos = ev*365
     print(f'{nev} {napos} napos')
 
+def ora(ev):
+    oras = ev*365*24
+    print(f'{nev} {oras} órás')
+
+def ora(ev):
+    perces = ev*365*24*60
+    print(f'{nev} {perces} perces')
+
+def ora(ev):
+    másodperces = ev*365*24*60*60
+    print(f'{nev} {másodperces} másodperces')
+
 nap(ev)
